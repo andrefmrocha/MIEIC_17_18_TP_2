ab902c00928c750adf95ee5ee6d154e650f0061f
andrefmrocha <andrefmrocha@live.com.pt>
2018-04-26 17:27:23 +0100

    Added Dictionary Class

---
+++ Dictionary.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andrefmrocha on 26-04-2018.
+//
+
+#include "Dictionary.h"
---
+++ Dictionary.h
@@ -0,0 +1,14 @@
+//
+// Created by andrefmrocha on 26-04-2018.
+//
+
+#ifndef MIEIC_17_18_TP_2_DICTIONARY_H
+#define MIEIC_17_18_TP_2_DICTIONARY_H
+
+
+class Dictionary {
+
+};
+
+
+#endif //MIEIC_17_18_TP_2_DICTIONARY_H
